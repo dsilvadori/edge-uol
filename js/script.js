7fe38c98c7dc516b858a25b26d39eb2a90753b67
Daniel Silva <devmaquina.ds@gmail.com>
2023-12-19 19:31:04 -0300

    Implementa rodape e menu mobile

--- js/script.js
+++ js/script.js
@@ -48,3 +48,15 @@ const swiperTopo = function(){
 }
   
 swiperInsights();
+
+// Menu hamburger
+var menuButton = document.getElementById("menu-button");
+var menu = document.getElementById("menu");
+
+menuButton.addEventListener("click", function() {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});
